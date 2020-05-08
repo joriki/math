e29d919a6af1222a9602ab64c8745c866881d20b
joriki <joriki27@gmail.com>
2020-05-09 00:42:25 +0200

    add random unit vectors

--- info/joriki/math/random/AugmentedRandom.java
+++ info/joriki/math/random/AugmentedRandom.java
@@ -12,7 +12,7 @@ public class AugmentedRandom extends Random {
     public double nextExponential(double lambda) {
         return -Math.log(nextDouble()) / lambda;
     }
-    
+
     public int nextPoisson(double lambda) {
         int result = 0;
         lambda = Math.exp(lambda);
@@ -22,5 +22,18 @@ public class AugmentedRandom extends Random {
                 return result;
             result++;
         }
+    }
+
+    public double [] nextUnitVector(int n) {
+        double [] unitVector = new double [n];
+        double norm = 0;
+        for (int i = 0;i < n;i++) {
+            unitVector [i] = nextGaussian();
+            norm += unitVector [i] * unitVector [i];
+        }
+        norm = 1 / Math.sqrt(norm);
+        for (int i = 0;i < n;i++)
+            unitVector [i] *= norm;
+        return unitVector;
     }
 }
