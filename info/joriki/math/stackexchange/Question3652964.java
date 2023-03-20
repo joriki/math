30a1a686ceec2e86cbb50ec52da54e85922cd9b6
joriki <joriki27@gmail.com>
2023-03-20 07:18:02 +0100

    simulate the probability that a bin with capacity 1 is empty when 5 bins with capacity 3 have been filled; see https://math.stackexchange.com/questions/3652964

---
+++ info/joriki/math/stackexchange/Question3652964.java
@@ -0,0 +1,26 @@
+package info.joriki.math.stackexchange;
+
+import java.util.Random;
+
+public class Question3652964 {
+    final static int ntrials = 100000000;
+    final static Random random = new Random();
+
+    public static void main(String [] args) {
+        long last = 0;
+        for (int n = 0;n < ntrials;n++) {
+            int [] counts = new int [5];
+            int count = 0;
+            for (;;) {
+                int bin = random.nextInt(6);
+                if (bin == 5)
+                    break;
+                if (++counts [bin] == 3 && ++count == 5) {
+                    last++;
+                    break;
+                }
+            }
+        }
+        System.out.println(last / (double) ntrials);
+    }
+}
