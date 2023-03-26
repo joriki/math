3544c3d137f1db806efaca5d7d9b62bd68c333f4
joriki <joriki27@gmail.com>
2023-03-26 19:29:43 +0200

    Estimate the expected number of turns and the distribution of its residues in short and long versions of a guessing game; see https://math.stackexchange.com/questions/4666493

---
+++ info/joriki/math/stackexchange/Question4666493.java
@@ -0,0 +1,29 @@
+package info.joriki.math.stackexchange;
+
+import java.util.Random;
+
+public class Question4666493 {
+    final static boolean longGame = false;
+    final static int modulus = 4;
+    final static int residue = 0;
+
+    final static long ntrials = 10000000;
+    final static Random random = new Random();
+
+    public static void main(String [] args) {
+        long count = 0;
+        long turns = 0;
+        for (long n = 0;n < ntrials;n++) {
+            for (int k = 2;;k++) {
+                turns++;
+                if ((random.nextInt(k) == 0) == longGame) {
+                    if (((k - 1) % modulus) == residue)
+                        count++;
+                    break;
+                }
+            }
+        }
+        System.out.println(turns / (double) ntrials);
+        System.out.println(count / (double) ntrials);
+    }
+}
